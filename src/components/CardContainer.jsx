91857ba492ef2e7017147d53838ea9b489abb3b0
ebrar <ebrkdmr1@gmail.com>
2023-12-07 22:44:03 +0300

    useState

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -2,22 +2,29 @@ import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import { data } from "../helpers/data";
-import Card from "react-bootstrap/Card";
+
+import Col from "react-bootstrap/esm/Col";
+import PlayerCard from "./PlayerCard";
+import { useState } from "react";
 
 const CardContainer = () => {
+  const [search, setSearch] = useState("");
   console.log(data);
+
   return (
     <>
-      <Form.Control placeholder="Search a player" className="w-50 m-auto" />
-      <Container>
-        <Row>
+      <Form.Control
+        placeholder="Search a player"
+        className="w-50 m-auto"
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <Container className="rounded-4 my-4 p-3 card-container">
+        <Row className="justify-content-center g-3">
           {data.map((player, i) => (
-            <Card>
-              <Card.Img variant="top" src={player.img} />
-              <Card.Footer>
-                <Card.Title>{player.name}</Card.Title>
-              </Card.Footer>
-            </Card>
+            <Col xl={3} lg={4} md={6} key={i}>
+              <PlayerCard {...player} />
+              {/* <PlayerCard player={player}  /> */}
+            </Col>
           ))}
         </Row>
       </Container>
